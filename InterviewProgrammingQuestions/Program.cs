11688a443bccaee88d50128d5e1a7c2c49e25f89
Manoj Karthigaisamy <manojkarthick1706@gmail.com>
2025-09-05 23:42:27 +0530

    Created modular console project to maintain all question in single place

---
+++ InterviewProgrammingQuestions/Program.cs
@@ -0,0 +1,14 @@
+﻿using InterviewProgrammingQuestions;
+using InterviewProgrammingQuestions.Questions.Async;
+
+Console.WriteLine("Choose a question to run:");
+Console.WriteLine("1. Async Question1");
+var choice = Console.ReadLine();
+
+IQuestion question = choice switch
+{
+    "1" => new LongProcessAsync(),
+    _ => null
+};
+
+question?.Run();
---
+++ InterviewProgrammingQuestions/Questions/Async/LongProcessAsync.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProgrammingQuestions.Questions.Async
+{
+    internal class LongProcessAsync : IQuestion
+    {
+        public void Run()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            Task<int> result1 = LongProcess1();
+            Task<int> result2 = LongProcess2();
+            Console.WriteLine("After two long processes.");
+
+            int val = await result1;
+            DisplayResult(val);
+
+            val = await result2;
+            DisplayResult(val);
+
+            Console.ReadKey();
+        }
+
+        static async Task<int> LongProcess1()
+        {
+            Console.WriteLine("LongProcess 1 Started");
+
+            await Task.Delay(8000);
+
+            Console.WriteLine("LongProcess 1 Completed");
+
+            return 10;
+        }
+
+        static async Task<int> LongProcess2()
+        {
+            Console.WriteLine("LongProcess 2 Started");
+
+            await Task.Delay(4000);
+
+            Console.WriteLine("LongProcess 2 Completed");
+
+            return 20;
+        }
+
+        static void DisplayResult(int val)
+        {
+            Console.WriteLine(val);
+        }
+    }
+
+
+}
