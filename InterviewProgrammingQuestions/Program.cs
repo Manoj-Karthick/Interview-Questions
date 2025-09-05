b50df37f9cb0b48d83a81df32e9265ae3628b72c
Manoj Karthigaisamy <manojkarthick1706@gmail.com>
2025-09-05 23:49:48 +0530

    Code cleanup

--- InterviewProgrammingQuestions/Program.cs
+++ InterviewProgrammingQuestions/Program.cs
@@ -2,12 +2,14 @@
 using InterviewProgrammingQuestions.Questions.Async;
 
 Console.WriteLine("Choose a question to run:");
-Console.WriteLine("1. Async Question1");
+Console.WriteLine("1. Long Process Async");
+Console.WriteLine("1. Method Call Async");
 var choice = Console.ReadLine();
 
 IQuestion question = choice switch
 {
     "1" => new LongProcessAsync(),
+    "2" => new MethodCallAsync(),
     _ => null
 };
 
